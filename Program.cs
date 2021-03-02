98999499ac7323b347bf28e99f43604bcbc96beb
Jaxel Rojas <jaxelr@gmail.com>
2021-03-02 05:24:11 -0400

    Sample insight.db usage on msprovider on 5.0

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Insight.Database;
+using System;
+using System.Data.SqlClient;
+
+namespace InsightNet50Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = new SqlConnection("Server=(local);Database=master;Trusted_Connection=true;");
+
+            Insight.Database.Providers.MsSqlClient.SqlInsightDbProvider.RegisterProvider();
+
+            foreach (string field in conn.QuerySql<string>("SELECT 'Hello' UNION SELECT 'World'"))
+            {
+                Console.WriteLine(field);
+            }
+
+            Console.Read();
+        }
+    }
+}
